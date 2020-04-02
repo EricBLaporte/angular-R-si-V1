9b3fa0b56d25157fa660d3a90da54644e82f1d35
EricBLaporte <laporte.eric.b@gmail.com>
2020-04-02 09:57:53 +0200

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { SourcesGIECComponent } from './sources-giec/sources-giec.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ],
-  declarations: [ AppComponent, MainpageComponent ],
+  declarations: [ AppComponent, MainpageComponent, SourcesGIECComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/sources-giec/sources-giec.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sources-giec',
+  templateUrl: './sources-giec.component.html',
+  styleUrls: ['./sources-giec.component.css']
+})
+export class SourcesGIECComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
