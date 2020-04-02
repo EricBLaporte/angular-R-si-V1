e4c1c6268e307273ca73a275b14e4e082fe5a2b9
EricBLaporte <laporte.eric.b@gmail.com>
2020-04-02 09:12:10 +0200

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GIECComponent } from './giec.component';
+import { appRoutingModule } from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,appRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, GIECComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
